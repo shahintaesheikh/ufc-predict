40359c4b7c57e5c3a7d17b86c7a5c810963d0fb9
shahinsheikh <shahinsheikh@ucsb.edu>
2025-10-26 15:54:55 -0700

    added new winner section overfitting still an issue

--- scrape-ufc-events.py
+++ scrape-ufc-events.py
@@ -5,88 +5,6 @@ from typing import List
 from bs4 import BeautifulSoup
 import pandas as pd
 
-"""
-
-RUN_ONLY_ONE = False
-RUN_LIMIT = 20
-IS_IC_DEBUG = False
-
-PROJECT_NAME = 'scrape-ufc-events'
-LOGGER = None
-
-
-
-def get_all_events() -> List[str]:
-    url = 'http://ufcstats.com/statistics/events/completed?page=all'
-
-    LOGGER.info('Fetching all completed events from %s', url)
-
-    try:
-        html = basic_request = (url, LOGGER)
-    except RuntimeError as e:
-        LOGGER.error('Failed to fetch events page: %s', format_error(e))
-        return []
-    
-    event_links = extract_event_links(html)
-    LOGGER.info('Found %d events', len(event_links))
-
-    return event_links
-
-def exctract_event_info(soup : BeautifulSoup) -> dict:
-    try:
-        event_name = soup.select_one('span.b-content__title-highlight').get_text(strip = True)
-        if event_name:
-            event_name = event_name.get_text(strip=True)
-    except AttributeError:
-        event_name = None
-    
-    try:
-        list_items = soup.select('li.b-list__box-list-item') 
-
-        event_date = None
-        event_loc = None
-
-        for item in list_items:
-            text = item.get_text(strip=True)
-            if 'Date:' in text:
-                event_date = text.split('Date:')[1].strip()
-                try:
-                    event_date = str(datetime.datetime.strptime(event_date, '%B %d, %Y').date())
-                except:
-                    pass
-            elif 'Location:' in text:
-                event_loc = text.split('Location:')[1].strip()
-    except:
-        event_date = None
-        event_loc = None
-    return {
-        'event_name' : event_name,
-        'event_date' : event_date,
-        'event_location' : event_loc
-    }
-
-def exctract_fight_data(fight_row, event_info:dict) -> dict:
-    fight_data = {}
-    
-    fight_data.update(event_info)
-
-    try:
-        cells = fight_row.select('td.b-fight-details__table-col')
-
-        fighters_cell = cells[0] if len(cells) > 0 else None
-
-        if fighters_cell:
-            fighter_links = fighters_cell.select('a')
-            if len(fighter_links) >= 2:
-                fight_data['fighter1'] = fighter_links[0].get_text(strip = True)
-                fight_data['fighter2'] = fighter_links[1].get_text(strip = True)
-
-        win_indicator = cells.select('b-flag__text').get_text(strip=True)
-        for cell in cells:
-
-
-"""
-
 response = requests.get("http://ufcstats.com/statistics/events/completed?page=all")
 
 def extract_event_links(html: str) -> List[str]:
@@ -124,6 +42,7 @@ event_dict = {"Event": ["-"],
        "Round": ["-"],
        "Time": ["-"],
        "Weight_Class": ["-"],
+       "Winner": ["-"],
        "Title": [0],
        "Fight_Bonus": [0],
        "Perf_Bonus": [0],
@@ -173,6 +92,8 @@ for i in range(min(len(event_links), RUN_LIMIT)):
         fighter_names = fight_details[1].find_all('p')
         fight_data.loc[rc, "Fighter_A"] = fighter_names[0].get_text(strip=True)
         fight_data.loc[rc, "Fighter_B"] = fighter_names[1].get_text(strip=True)
+
+        fight_data.loc[rc, "Winner"] = fighter_names[0].get_text(strip=True)
         #fight statistics
         KD = fight_details[2].find_all('p')
         fight_data.loc[rc, "Fighter_A_KD"] = KD[0].get_text(strip=True)
